8d46ed5221ad010fc177e2f82b16841e59a97ce8
Diego Chinellato <chine.diego@gmail.com>
2020-08-29 18:54:45 +0200

    implemented randomization test

--- utils.py
+++ utils.py
@@ -56,15 +56,66 @@ def compare_model_error(eval_data, model_names, metric='ndcg@10', plot=False, sa
         plt.savefig(filename)
     return metric_150_trees, metric_all_trees
 
-
-def randomization_test(X, y_true, model_a, model_b, ndcg_pos=[10], n_perm=10000):
+@Timeit('Randomization test')
+def randomization_test(X, y_true, group, model_a, model_b, metric='ndcg@10', n_perm=10000):
     """
     This methods performs a randomization test (i.e. computes statistical significance)
     of the performance difference between model_a and model_b
     """
+    # Get predictions 
     y_pred_a = model_a.predict(X)
     y_pred_b = model_b.predict(X)
-    for pos in ndcg_pos:
-        ndcg_a = ndcg_score(y_score=y_pred_a, y_true=y_true, k=pos)
-        ndcg_b = ndcg_score(y_score=y_pred_b, y_true=y_true, k=pos)
+    
+    # per-query evaluation of given metric
+    idx_start = 0
+    n_queries = len(group)
+    query_scores_a = np.zeros(n_queries, dtype=np.float32)
+    query_scores_b = np.zeros(n_queries, dtype=np.float32)
+    for i, query_size in enumerate(group):
+        if metric.startswith('ndcg@'):
+            idx_end = idx_start + query_size
+            cutoff = int(metric.split('@')[-1])
+            # ndcg_scores requires array of shape (n_samples, n_labels)
+            query_scores_a[i] = ndcg_score(y_score=[y_pred_a[idx_start:idx_end]], 
+                                           y_true=[y_true[idx_start:idx_end]], k=cutoff)
+            query_scores_b[i] = ndcg_score(y_score=[y_pred_b[idx_start:idx_end]], 
+                                           y_true=[y_true[idx_start:idx_end]], k=cutoff)
+            idx_start += query_size
+        else:
+            raise ValueError('Wrong or unsupported metric')
+    
+    # randomization test
+    if np.mean(query_scores_a) > np.mean(query_scores_b):
+        best_scores = query_scores_a
+        worst_scores = query_scores_b
+    else:
+        best_scores = query_scores_b
+        worst_scores = query_scores_a
+    
+    diff = np.mean(best_scores) - np.mean(worst_scores)
+    abs_diff = np.abs(diff)
+    p1, p2 = 0.0, 0.0
+    best_sum, worst_sum = np.sum(best_scores), np.sum(worst_scores)
+    
+    for i in range(n_perm):
+        # select a random subset 
+        subset = np.random.choice([False, True], n_queries)
         
+        best_sub_sum = np.sum(best_scores[subset])
+        worst_sub_sum = np.sum(worst_scores[subset])
+        
+        # compute avg performance of randomized models
+        best_mean = (best_sum - best_sub_sum + worst_sub_sum) / float(n_queries)
+        worst_mean = (worst_sum - worst_sub_sum + best_sub_sum) / float(n_queries)
+        
+        delta = best_mean - worst_mean
+        
+        if delta >= diff:
+            p1 += 1
+        if np.abs(delta) >= abs_diff:
+            p2 += 1
+            
+    p1 /= n_perm
+    p2 /= n_perm
+    
+    return p1, p2
