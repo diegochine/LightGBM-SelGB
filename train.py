e2b3d8aae2bd55e8c1cd6a0b7acab834ffd44b97
Diego Chinellato <chine.diego@gmail.com>
2020-09-03 19:03:34 +0200

    training script ready for slurm

--- train.py
+++ train.py
@@ -3,10 +3,10 @@
 import os
 import sys
 
-import lightgbm as lgbm
+import lightgbm as lgb
 
 from LGBMSelGB import LGBMSelGB
-from utils import Timeit, compare_model_error, load_data
+from utils import load_data, dump_obj
 
 base_path = os.getcwd()
 datasets_path = os.path.join(base_path, 'datasets')
@@ -16,9 +16,10 @@ test_file = os.path.join(datasets_path, "test.txt")
 output_path = os.path.join(base_path, 'output')
 models_path = os.path.join(output_path, 'models')
 results_path = os.path.join(output_path, 'results')
+logs_path = os.path.join(output_path, 'logs')
 
 
-def train():
+def train(algo):
     print('Loading data')
     train_data, train_labels, train_query_lens = load_data(train_file)
     print('Training set loaded')
@@ -27,7 +28,7 @@ def train():
     test_data, test_labels, test_query_lens = load_data(test_file)
     print('Testing set loaded')
 
-    train_set = lgbm.Dataset(train_data, label=train_labels, group=train_query_lens)
+    train_set = lgb.Dataset(train_data, label=train_labels, group=train_query_lens)
     eval_set = [(train_data, train_labels),
                 (valid_data, valid_labels),
                 (test_data, test_labels)]
@@ -35,7 +36,7 @@ def train():
     eval_names = ['train', 'valid', 'test']
     valid_sets = []
     for i, valid_data in enumerate(eval_set):
-        ds = lgbm.Dataset(valid_data[0], valid_data[1], group=eval_group[i], reference=train_set)
+        ds = lgb.Dataset(valid_data[0], valid_data[1], group=eval_group[i], reference=train_set)
         valid_sets.append(ds)
 
     params = {
@@ -47,45 +48,33 @@ def train():
         'ndcg_eval_at': 10
     }
 
-    @Timeit('LGBM train')
-    def train_lgbm_model():
-        evals_result = {}
-        lgb_model = lgbm.train(params, train_set, num_boost_round=1000,
-                               valid_sets=valid_sets, valid_names=eval_names,
-                               verbose_eval=5, evals_result=evals_result)
-        return lgb_model, evals_result
-
-    lgb_model, lgb_info = train_lgbm_model()
-
     strategies = ('fixed', 'random_iter', 'random_query', 'inverse',
-                  'wrong_neg', 'equal_size', 'delta')
+                  'false_positives', 'equal_size', 'delta')
+    n_estimators = 1000
+    early_stopping_rounds = 100
+    verbose = 5
 
-    selgb_base = LGBMSelGB(n_estimators=1000, n_iter_sample=1, p=0.01, method='fixed')
-    selgb_base.fit(train_data, train_labels, train_query_lens,
-                   eval_set=eval_set, eval_group=eval_group, eval_names=eval_names,
-                   verbose=5)
+    if algo in ('lgbm base', 'lgbm goss'):
+        if algo == 'lgbm goss':
+            params['boosting'] = 'goss'
+        eval_results = {}
+        model = lgb.train(params, train_set, num_boost_round=n_estimators,
+                          valid_sets=valid_sets, valid_names=eval_names,
+                          verbose_eval=verbose, evals_result=eval_results,
+                          early_stopping_rounds=early_stopping_rounds)
+    elif algo in strategies:
+        model = LGBMSelGB(n_estimators=n_estimators, n_iter_sample=10, p=0.01, method=algo)
+        model.fit(train_data, train_labels, train_query_lens,
+                  eval_set=eval_set, eval_group=eval_group, eval_names=eval_names,
+                  verbose=verbose)
+        eval_results = model.get_eval_result()
+    else:
+        raise ValueError('algo parameter is wrong')
 
-    compare_model_error(eval_data=[lgb_info, selgb_base.get_eval_result()],
-                        model_names=['LightGBM', 'Selgb'])
-
-    lgb_model.save_model(os.path.join(models_path, 'lightgbm.txt'))
-    selgb_base.save_model(os.path.join(models_path, 'selgb_base.txt'))
-
-
-def test_load():
-    import numpy as np
-    print('Loading data')
-    train_data, train_labels, train_query_lens = load_data(train_file)
-    print('Training set loaded')
-    print('Shape:', train_data.shape)
-    final_idx = np.sort(np.random.choice(list(range(train_data.shape[0])), size=int(train_data.shape[0]*0.1), replace=False))
-    sliced = train_data[final_idx]
-    print('Sliced1 shape:', sliced.shape)
-    final_idx = np.sort(np.random.choice(list(range(train_data.shape[0])), size=int(train_data.shape[0]*0.01), replace=False))
-    sliced = train_data[final_idx]
-    print('Sliced2 shape:', sliced.shape)
+    model.save_model(os.path.join(models_path, algo + '.txt'))
+    dump_obj(eval_results, results_path, algo)
 
 
 if __name__ == '__main__':
-    test_load()
+    train(sys.argv[1])
     sys.exit(0)
