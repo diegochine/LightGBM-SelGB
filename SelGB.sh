9bc08a1908e8c01c171ea599355729a965a0473b
Diego Chinellato <chine.diego@gmail.com>
2020-09-03 19:02:18 +0200

    parallel training tasks

--- SelGB.sh
+++ SelGB.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-#sbatch --job-name=selgb.job
+#SBATCH --job-name=ltr-train.job
+#SBATCH --ntasks=5
+#SBATCH --cpus-per-task=2
 
-python3.6 /rhome/hpc20/867637/train.py
+srun --ntasks=1 python3.6 /rhome/hpc20/867637/train.py "lgbm base" &
+srun --ntasks=1 python3.6 /rhome/hpc20/867637/train.py "lgbm goss" &
+srun --ntasks=1 python3.6 /rhome/hpc20/867637/train.py "fixed" &
+srun --ntasks=1 python3.6 /rhome/hpc20/867637/train.py "false_positives" &
+srun --ntasks=1 python3.6 /rhome/hpc20/867637/train.py "equal_size" &
+wait
