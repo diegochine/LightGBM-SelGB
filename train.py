5e442550ff05d9d4fa05fb237c450c20d0c20f5c
Diego Chinellato <chine.diego@gmail.com>
2020-09-03 19:39:23 +0200

    no ok now it's ready for slurm

--- train.py
+++ train.py
@@ -10,9 +10,10 @@ from utils import load_data, dump_obj
 
 base_path = os.getcwd()
 datasets_path = os.path.join(base_path, 'datasets')
-train_file = os.path.join(datasets_path, "train.txt")
-valid_file = os.path.join(datasets_path, "vali.txt")
-test_file = os.path.join(datasets_path, "test.txt")
+istellax_path = os.path.join(datasets_path, "istella-X")
+train_file = os.path.join(istellax_path, "train.txt")
+valid_file = os.path.join(istellax_path, "vali.txt")
+test_file = os.path.join(istellax_path, "test.txt")
 output_path = os.path.join(base_path, 'output')
 models_path = os.path.join(output_path, 'models')
 results_path = os.path.join(output_path, 'results')
@@ -20,6 +21,7 @@ logs_path = os.path.join(output_path, 'logs')
 
 
 def train(algo):
+    print('Chosen algo is:', algo)
     print('Loading data')
     train_data, train_labels, train_query_lens = load_data(train_file)
     print('Training set loaded')
@@ -28,17 +30,11 @@ def train(algo):
     test_data, test_labels, test_query_lens = load_data(test_file)
     print('Testing set loaded')
 
-    train_set = lgb.Dataset(train_data, label=train_labels, group=train_query_lens)
     eval_set = [(train_data, train_labels),
                 (valid_data, valid_labels),
                 (test_data, test_labels)]
     eval_group = [train_query_lens, valid_query_lens, test_query_lens]
     eval_names = ['train', 'valid', 'test']
-    valid_sets = []
-    for i, valid_data in enumerate(eval_set):
-        ds = lgb.Dataset(valid_data[0], valid_data[1], group=eval_group[i], reference=train_set)
-        valid_sets.append(ds)
-
     params = {
         'objective': 'lambdarank',
         'max_position': 10,
@@ -57,22 +53,46 @@ def train(algo):
     if algo in ('lgbm base', 'lgbm goss'):
         if algo == 'lgbm goss':
             params['boosting'] = 'goss'
-        eval_results = {}
-        model = lgb.train(params, train_set, num_boost_round=n_estimators,
-                          valid_sets=valid_sets, valid_names=eval_names,
-                          verbose_eval=verbose, evals_result=eval_results,
-                          early_stopping_rounds=early_stopping_rounds)
+            train_set = lgb.Dataset(train_data, label=train_labels, group=train_query_lens)
+            eval_results = {}
+            valid_sets = [train_set]
+            for i, data in enumerate(eval_set[1:]):
+                ds = lgb.Dataset(data[0], data[1], group=eval_group[1:][i], reference=train_set)
+                valid_sets.append(ds)
+            model = lgb.train(params, train_set, num_boost_round=n_estimators,
+                              valid_sets=valid_sets, valid_names=eval_names,
+                              verbose_eval=verbose, evals_result=eval_results,
+                              early_stopping_rounds=early_stopping_rounds)
+        else:
+            for n in (100, 500, 1000, 2500, ''):
+                ds_path = os.path.join(datasets_path, 'istella-X{}'.format(n))
+                file = os.path.join(ds_path, 'train.txt')
+                train_data, train_labels, train_query_lens = load_data(file)
+                print('Training set loaded (istella-X{})'.format(n))
+                eval_results = {}
+                train_set = lgb.Dataset(train_data, label=train_labels, group=train_query_lens)
+                valid_sets = [train_set]
+                for i, data in enumerate(eval_set[1:]):
+                    ds = lgb.Dataset(data[0], data[1], group=eval_group[1:][i], reference=train_set)
+                    valid_sets.append(ds)
+                model = lgb.train(params, train_set, num_boost_round=n_estimators,
+                                  valid_sets=valid_sets, valid_names=eval_names,
+                                  verbose_eval=verbose, evals_result=eval_results,
+                                  early_stopping_rounds=early_stopping_rounds)
+                model.save_model(os.path.join(models_path, algo + str(n) + '.txt'))
+                dump_obj(eval_results, results_path, algo + str(n))
     elif algo in strategies:
         model = LGBMSelGB(n_estimators=n_estimators, n_iter_sample=10, p=0.01, method=algo)
         model.fit(train_data, train_labels, train_query_lens,
                   eval_set=eval_set, eval_group=eval_group, eval_names=eval_names,
-                  verbose=verbose)
+                  verbose=verbose, early_stopping_rounds=early_stopping_rounds)
         eval_results = model.get_eval_result()
     else:
         raise ValueError('algo parameter is wrong')
 
-    model.save_model(os.path.join(models_path, algo + '.txt'))
-    dump_obj(eval_results, results_path, algo)
+    if algo != 'lgbm base':
+        model.save_model(os.path.join(models_path, algo + '.txt'))
+        dump_obj(eval_results, results_path, algo)
 
 
 if __name__ == '__main__':
