06ecf5bb767db45834df08bd5bde849e64c40126
Diego Chinellato <chine.diego@gmail.com>
2020-09-10 16:46:16 +0200

    Update SelGB.sh

--- SelGB.sh
+++ SelGB.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-#SBATCH --job-name=ltr-train.job
-#SBATCH --ntasks=5
+#SBATCH --job-name=selgb
+#SBATCH --ntasks=3
 #SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=400000
 
-srun --ntasks=1 python3.6 /rhome/hpc20/867637/train.py "lgbm base" &
-srun --ntasks=1 python3.6 /rhome/hpc20/867637/train.py "lgbm goss" &
-srun --ntasks=1 python3.6 /rhome/hpc20/867637/train.py "fixed" &
-srun --ntasks=1 python3.6 /rhome/hpc20/867637/train.py "false_positives" &
-srun --ntasks=1 python3.6 /rhome/hpc20/867637/train.py "equal_size" &
+
+srun --ntasks=1 --job-name=fixed python3.6 /rhome/hpc20/867637/train.py "fixed" &
+srun --ntasks=1 --job-name=falpos python3.6 /rhome/hpc20/867637/train.py "false_positives" &
+srun --ntasks=1 --job-name=eqsize python3.6 /rhome/hpc20/867637/train.py "equal_size" &
 wait
